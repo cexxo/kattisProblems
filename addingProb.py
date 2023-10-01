1f31cc3202a8a6e67b8f0c8efb892c46374131ef
cexxouser.email <criscifrancesco00@gmail.com>
2023-10-01 15:34:49 +0200

    Solutions

---
+++ addingProb.py
@@ -0,0 +1,5 @@
+a = input().split()
+if(int(a[0])+int(a[1])==int(a[2])):
+    print("correct!")
+else:
+    print("wrong!")
--- multtable.py
+++ multtable.py
@@ -4,7 +4,12 @@ n= int(temp[0])
 m = int(temp[1])
 counter = 0
 b = m
-for i in range(1,n+1):
-    if b%i == 0 and b/i <=n:
-        counter +=1
+if m > n:
+    for i in range(1,n+1):
+        if b%i == 0 and b/i <=n:
+            counter +=1
+else:
+    for i in range(1,m+1):
+        if b%i == 0 and b/i <=m:
+            counter +=1
 print(counter)
